0407978cf89e84e75b1227e27f7ce99a3403ca96
vakenn <khatteli@yahoo.fr>
2026-03-02 08:10:46 +0100

    remove cors middleware

--- app/main.py
+++ app/main.py
@@ -1,19 +1,7 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from app.routers import recommendation
 
 app = FastAPI(title="what to cook API", version="1.0.0")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "https://cook-assistant-beryl.vercel.app"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 app.include_router(recommendation.router, prefix="/api", tags=["Chat"])
 
